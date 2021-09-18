b04383f2531aaac42b1d0926746cfc79ea3451a0
David Carter <dajocarter@gmail.com>
2021-09-17 20:46:23 -0700

    add SWRConfig to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        fallback: pageProps.fallback,
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
 export default MyApp
