5ef664906d5189c8f67f329cfc2a70b1b021f8ae
David Carter <dajocarter@gmail.com>
2021-09-17 20:55:20 -0700

    every page needs to pass down fetched data in props

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { getWPMedia } from '../lib/wordpress'
 
 const Home: NextPage = () => {
   return (
@@ -70,3 +71,14 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getStaticProps () {
+  const logo = await getWPMedia('purofluxlogo_white_2x')
+  return {
+    props: {
+      fallback: {
+        '/api/media/purofluxlogo_white_2x': logo
+      }
+    }
+  }
+}
