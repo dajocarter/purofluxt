7ac5a53e3b4e74571fcf624427f728172e766ddb
David Carter <dajocarter@gmail.com>
2021-09-17 21:17:09 -0700

    create a function to fetch WP menu and add/create wp-types

--- lib/wordpress.ts
+++ lib/wordpress.ts
@@ -1,5 +1,26 @@
-export async function getWPMedia (mediaSlug: string) {
+import type { WP_REST_API_Attachment, WP_REST_API_Post } from 'wp-types'
+
+export async function getWPMedia (mediaSlug: string): Promise<{ status: number; data: WP_REST_API_Attachment}> {
   const mediaResponse = await fetch(`${process.env.WP_API_URL}/wp/v2/media?slug=${mediaSlug}`)
   const mediaData = await mediaResponse.json()
   return { status: mediaResponse.status, data: mediaData?.[0] }
+}
+
+export type WP_REST_API_Menu = {
+  term_id: number;
+  name: string;
+  slug: string;
+  term_group: number;
+  term_taxonomy_id: number;
+  taxonomy: string;
+  description: string;
+  parent: number;
+  count: number;
+  filter: string;
+  items: WP_REST_API_Post[];
+}
+export async function getWPMenu (menuSlug: string): Promise<{ status: number; data: WP_REST_API_Menu; }> {
+  const menuResponse = await fetch(`${process.env.WP_API_URL}/menus/v1/menus/${menuSlug}`)
+  const menuData = await menuResponse.json()
+  return { status: menuResponse.status, data: menuData }
 }
