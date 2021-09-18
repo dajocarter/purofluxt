8ccc0ae067245e2de70e778cd09252419148d390
David Carter <dajocarter@gmail.com>
2021-09-17 20:55:34 -0700

    add header component to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
+import Header from '../components/header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
       }}
     >
+      <Header />
       <Component {...pageProps} />
     </SWRConfig>
   )
